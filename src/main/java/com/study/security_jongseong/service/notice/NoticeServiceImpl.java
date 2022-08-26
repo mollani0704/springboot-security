a4e4b42e27a6601a0b811dac62d420d50946112c
mollani <mollani0704@gmail.com>
2022-08-26 21:43:30 +0900

    [study] 22.08.26 웹개발 스프릥 시큐리티 게시판 page 이동 구현

--- src/main/java/com/study/security_jongseong/service/notice/NoticeServiceImpl.java
+++ src/main/java/com/study/security_jongseong/service/notice/NoticeServiceImpl.java
@@ -63,9 +63,12 @@ public class NoticeServiceImpl implements NoticeService{
 		
 		Notice notice = null;
 		
+		String noticeTitle = addNoticeReqDto.getNoticeTitle();
 		
+		for(int i = 0; i < 200; i++) {
+			
 		notice = Notice.builder()
-				.notice_title(addNoticeReqDto.getNoticeTitle())
+				.notice_title(noticeTitle + "_" + i)
 				.user_code(addNoticeReqDto.getUserCode())
 				.notice_content(addNoticeReqDto.getIr1())
 				.build();
@@ -99,6 +102,7 @@ public class NoticeServiceImpl implements NoticeService{
 		noticeRepository.saveNoticeFiles(noticeFiles);
 		
 	
+		}
 	}
 		return notice.getNotice_code();
 	}
--- src/main/resources/static/js/notice/notice-list.js
+++ src/main/resources/static/js/notice/notice-list.js
@@ -3,7 +3,7 @@ const searchButton = document.querySelector(".search-button");
 
 let nowPage = 1;
 
-load(nowPage)
+load(nowPage);
 
 function load(nowPage) {
 	const searchFlag = document.querySelector(".search-select").value;
@@ -20,7 +20,13 @@ function load(nowPage) {
 		dataType: "json",
 		success: (response) => {
 			console.log(response)
-			getList(response.data);
+			if(response.data != null) {
+				getList(response.data);
+				getPageNumbers(response.data[0].totalNoticeCount);				
+			} else {
+				getList(new Array());
+				getPageNumbers(0);
+			}
 		},
 		error: (error) => {
 			console.log(error);
@@ -41,7 +47,71 @@ function getList(list) {
 	         <td>${notice.noticeCount}</td>
 	     </tr>
 		`
+	});
+	
+	const noticeRows = document.querySelectorAll(".notice-row");
+	
+	noticeRows.forEach((row) => {		
+		row.onclick = () => {
+			const noticeCode = row.querySelectorAll("td")[0].textContent;
+			location.href = "/notice/detail/" + noticeCode;
+		}
 	})
+	
+}
+
+function getPageNumbers(totalNoticeCount) {
+	const totalPageCount = totalNoticeCount % 10 == 0 ? totalNoticeCount / 10 : (totalNoticeCount / 10) + 1;
+	let startIndex = nowPage % 5 == 0 ? nowPage - 4 : nowPage - (nowPage % 5) + 1;
+	let endIndex = startIndex + 4 <= totalPageCount ? startIndex + 4 : totalPageCount;
+
+	const pageButtons = document.querySelector(".page-buttons")
+	
+	pageButtons.innerHTML = ``;
+	
+	if(startIndex != 1) {
+		pageButtons.innerHTML += `
+			<button type="button" class="page-button pre">&lt;</button>
+		`;
+	}
+	
+	for(let i = startIndex; i <= endIndex; i++) {
+		pageButtons.innerHTML += `
+			<button type="button" class="page-button">${i}</button>
+		`
+	}
+	
+	if(startIndex != totalPageCount) {
+		pageButtons.innerHTML += `
+			<button type="button" class="page-button next">&gt;</button>
+		`;
+	}
+	
+	if(startIndex != 1) {
+		const prePageButton = document.querySelector(".pre");
+		prePageButton.onclick = () => {
+			nowPage = startIndex - 1;
+			load(nowPage);
+		}
+	}
+	
+	if(startIndex != totalPageCount) {
+		const nextPageButton = document.querySelector(".next");
+		nextPageButton.onclick = () => {
+			nowPage = endIndex - 1;
+			load(nowPage);
+		}
+	}
+	
+	const pageNumberButtons = document.querySelectorAll(".page-button");
+	pageNumberButtons.forEach(button => {
+		if(button.textContent != "<" && button.textContent != ">") {
+			button.onclick = () => {
+				nowPage = button.textContent;
+				load(nowPage);
+			}
+		}
+	});
 }
 
 function getWriterButton() {
